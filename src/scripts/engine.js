3ae3e51e971cbc5509ba8c3408b809f2a61496a3
edinardo-pereira <edinardopereyra@gmail.com>
2024-10-10 13:36:42 -0300

    jogo-da-memoria

---
+++ src/scripts/engine.js
@@ -0,0 +1,74 @@
+const emojis = [
+    "💞",
+    "💞",
+    "💨",
+    "💨",
+    "💤",
+    "💤",
+    "😎",
+    "😎",
+    "🤡",
+    "🤡",
+    "💩",
+    "💩",
+    "😹",
+    "😹",
+    "🦄",
+    "🦄",
+    "🐶",
+    "🐶",
+    "🐰",
+    "🐰",
+    "🥰",
+    "🥰",
+    "🦅",
+    "🦅",
+    "👰",
+    "👰",
+    "🧜‍♀️",
+    "🧜‍♀️",
+    "🚴‍♂️",
+    "🚴‍♂️",
+    "💖",
+    "💖",
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    box.onclick = handleClick;
+    document.querySelector(".game").
+    appendChild(box);
+}
+
+function handleClick() {
+    if(openCards.length < 2){
+        this.classList.add("boxOpen");
+        openCards.push(this);
+    }
+
+    if(openCards.length == 2){
+        setTimeout(checkMatch, 500);
+    }
+}
+
+function checkMatch(){
+    if(openCards[0].innerHTML === openCards[1].innerHTML){
+        openCards[0].classList.add("boxMatch");
+        openCards[1].classList.add("boxMatch")
+    }else{
+        openCards[0].classList.remove("boxOpen");
+        openCards[1].classList.remove("boxOpen");
+    }
+    openCards = [];
+
+    if(document.querySelectorAll(".boxMatch").length === emojis.length){
+        alert("Jogo Finalizado !");
+    }
+
+}
+
